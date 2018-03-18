d38289e90667a0630d6bd0d01bb016505cb180cd
Christian Paling <christian.paling@googlemail.com>
2018-03-18 18:55:38 +0100

    Add some usage print statements

--- cmd/gom/main.go
+++ cmd/gom/main.go
@@ -8,6 +8,11 @@ import (
 )
 
 func main() {
+	if len(os.Args) <= 1 {
+		printUsage()
+		return
+	}
+
 	command, err := commands.Select(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
@@ -17,4 +22,12 @@ func main() {
 	if err = command.Run(os.Args[2:]...); err != nil {
 		fmt.Println(err)
 	}
+}
+
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("\tgom init\t\t\t-\tInitializes the db folder and the schema_migrations table")
+	fmt.Println("\tgom generate <migration>\t-\tCreate up.sql and down.sql for the specified migration name")
+	fmt.Println("\tgom migrate\t\t\t-\tExecutes all up.sql files of all migrations that were not migrated yet")
+	fmt.Println("\tgom rollback\t\t\t-\tExecutes the down.sql file of the last migrated migration")
 }
