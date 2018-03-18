bb99a6295f6f1f420813e2e5f3a3384c31679bc3
Christian Paling <christian.paling@googlemail.com>
2018-03-18 18:36:16 +0100

    Finish main.go for final executable

--- cmd/gom/main.go
+++ cmd/gom/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/bakku/gom/commands"
+)
+
+func main() {
+	command, err := commands.Select(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if err = command.Run(os.Args[2:]...); err != nil {
+		fmt.Println(err)
+	}
 }
